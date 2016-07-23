e5f991ff33331b38aa8b831e36aa3665f6c718df
DmitriyRelkin <dmitrij.relkin@gmail.com>
2016-07-24 00:56:40 +0300

    added bower-components and node-modules to git ignore

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -8,6 +8,15 @@ app.controller("mainHeaderCtrl", function($scope) {
         $scope.url = $scope.headerView;
     }
 });
+app.controller("mainFooterCtrl", function($scope) {
+    $scope.footerView = "footer.html";
+
+    $scope.url = $scope.footerView;
+    
+    $scope.showHeader = function() {
+        $scope.url = $scope.footerView;
+    }
+});
 // -=-=-=-=-=-=-=-=-=-=-Router=-=--=-=-=-=-=-=-=-=-=-=-
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
@@ -52,4 +61,25 @@ app.controller("galaryCtrl", function($scope) {
     $scope.showPhoto = function(index) {
         $scope.photoSrc = $scope.photos[index];
     };
+});
+// ==============BisinessBlock=============================
+
+app.controller("bisinessBlockCtrl", function($scope) {
+    $scope.serviceBlock = [
+        {srcImg: "build/img/icon-service-1.png", header:"BUSINESS CONSULTING", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"},
+        {srcImg: "build/img/icon-service-2.png", header:"ENTERPRISE APPLICATION", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"},
+        {srcImg: "build/img/icon-service-3.png", header:"NEW STRATEGY", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"},
+        {srcImg: "build/img/icon-service-4.png", header:"IT MANAGEMENT", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"},
+        {srcImg: "build/img/icon-service-5.png", header:"OUTSOURSING", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"},
+        {srcImg: "build/img/icon-service-6.png", header:"SALES & MARKETING", paragraph: "Vestibulum quis felis ut enim aliquam iaculis. Nullam pharetra tortor at quam viverra volutpat. Phasellus vel faucibus dolor. Curabitur"}
+    ];
+});
+
+// ==============Strategy-Block============================
+app.controller("strategyCtrl", function($scope) {
+    $scope.strategyBlock = [
+        {srcImg: "build/img/page1_img1.jpg", header:"QUESTIONS TO ASK:", item1:"Analysing your organisation business model", item2:"Identifying development opportunities", item3:"Defining your objectives", item4:"Identifying the key factors for success"},
+        {srcImg: "build/img/page1_img2.jpg", header:"THE COMPANY'S ROLE:", item1:"Identifying development opportunities", item2: "Analysing your organisation business model", item3:"Defining your objectives", item4:"Identifying the key factors for success"},
+        {srcImg: "build/img/page1_img3.jpg", header:"OUR KEY OFFERS:", item1:"Defining your objectives", item2: "Analysing your organisation business model", item3:"Identifying development opportunities",  item4:"Identifying the key factors for success"}
+    ];
 });
