c26550e3aaac103515623682dc98d0a2182c0db2
DmitriyRelkin <dmitrij.relkin@gmail.com>
2016-07-26 13:48:59 +0300

    amendment nesting

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var sass = require('gulp-sass');
  
 // gulp.task('sass:build', function () {
 gulp.task('sass', function () {
-  return gulp.src('./styles/main.scss')
+  return gulp.src('./website/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./website/consalting/assets/css')); 
+    .pipe(gulp.dest('./website/css')); 
 });
  
 gulp.task('sass:watch', function () {
-	gulp.watch("./styles/**", ['sass']);
+	gulp.watch("./website/scss/**", ['sass']);
 });
 
 // gulp.task('sass', ['sass:build', 'sass:watch']);
