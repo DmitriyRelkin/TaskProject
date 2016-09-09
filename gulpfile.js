9b1db09f898f99ca18f253e155822c5905b7d511
DmitriyRelkin <dmitrij.relkin@gmail.com>
2016-09-09 17:29:47 +0300

    setting NgDocs

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@
 
 var gulp = require('gulp');
 
-var sass = require('gulp-sass');
+var sass       = require('gulp-sass');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var gutil = require('gulp-util');//
-var buffer = require('vinyl-buffer');
-var debug = require('gulp-debug');
-var watch = require('gulp-watch');
-var uglify = require('gulp-uglify');
-var server = require('gulp-develop-server');
+var source     = require('vinyl-source-stream');
+var gutil      = require('gulp-util');//
+var buffer     = require('vinyl-buffer');
+var debug      = require('gulp-debug');
+var watch      = require('gulp-watch');
+var uglify     = require('gulp-uglify');
+var server     = require('gulp-develop-server');
+var ngdocs     = require ('gulp-ngdocs');
+
 
 var  sourceFile = './website/js/app.js';
 var  destFile = 'bundle.js';
@@ -46,6 +48,13 @@ gulp.task('server:start', function() {
 
 gulp.task('watch', function () {
   gulp.watch("website/js/**/*.js", ['browserify']);
+});
+
+gulp.task('ngdocs', [], function () {
+  var gulpDocs = require('gulp-ngdocs');
+  return gulp.src("website/js/**/*.js")
+    .pipe(gulpDocs.process())
+    .pipe(gulp.dest('./website/docs'));
 });
 
 gulp.task('default', ['browserify', 'watch', "sass:watch"]);
--- website/js/bundle.js
+++ website/js/bundle.js
@@ -66,6 +66,13 @@ module.exports = function(module) {
 
 },{}],3:[function(require,module,exports){
 module.exports = function(module) {
+  /**
+  * @ngdoc directive
+  * @name module.controller:blogContentCtrl
+  * @description
+  * This is the blog content controller.
+  * He keeps the array, in which objects with links of pictures, headers, and paragraphs with text.
+  **/
   module.controller("blogContentCtrl", function($scope) {
       $scope.blogContent = [
           {depiction: "website/images/img-14.jpg", header:"NULLA NEC LIGULA RISUS", paragraph: "Praesent sit amet ligula in ante aliquam pulvinar ac sit amet magna. Sed ut nisi at neque faucibus vehicula ut scelerisque justo. Phasellus semper faucibus tellus in lobortis. Nam odio metus, lacinia nec tristique ut, rutrum non mi. Aliquam suscipit consequat nibh, vel egestas dolor interdum nec. Praesent mattis odio mi, at vulputate ligula varius sed. Nulla a nulla viverra, ullamcorper orci vitae, tempor felis. In tempus justo vel felis posuere, a hendrerit nisi tincidunt. Aenean lobortis luctus orci, vitae euismod purus imperdiet placerat."},
