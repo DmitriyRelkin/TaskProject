5d477ab39349fbabee7d351d8835665d5ba19177
DmitriyRelkin <dmitrij.relkin@gmail.com>
2016-09-29 21:19:23 +0300

    Correction protractor config

--- protractor.config.js
+++ protractor.config.js
@@ -1,27 +1,17 @@
-// An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
   onPrepare: function() {
     var SpecReporter = require('jasmine-spec-reporter');
-    // add jasmine spec reporter
     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
   },
 
-  // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
   specs: ['website/test/protractor_tests/**/*.spec.js'],
 
-  // allScriptsTimeout: 30000,
-
-  // params: {login: { user: ‘Alex Belik’, password: ‘123456’, checkpassword: ‘123456’, subject: 'subject', message: 'message'}},
-  // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000
